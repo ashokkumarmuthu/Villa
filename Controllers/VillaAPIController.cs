2e1a15702dbc46a65052a26b4d7e060c26181cf9
ashok.kumar <ashokmuthu39@gmail.com>
2023-09-14 01:42:49 +0530

    Add. First endpoint

---
+++ Controllers/VillaAPIController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using Microsoft.AspNetCore.Mvc;
+using Villa_Api.Data;
+using Villa_Api.Model;
+using Villa_Api.Model.DTO;
+
+namespace Villa_Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class VillaAPIController : ControllerBase
+	{
+        [HttpGet]
+		public IEnumerable<VillaDTO> GetVillas()
+		{
+			return VillaStore.VillaList;
+		}
+        [HttpGet("{id:int}")]
+        public VillaDTO GetVilla(int id)
+        {
+            return VillaStore.VillaList.FirstOrDefault(u=>u.id==id);
+        }
+    }
+}
+
---
+++ Model/DTO/VillaDTO.cs
@@ -0,0 +1,10 @@
+﻿using System;
+namespace Villa_Api.Model.DTO
+{
+	public class VillaDTO
+	{
+        public int id { get; set; }
+        public string name { get; set; }
+    }
+}
+
